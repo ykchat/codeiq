fdf9b6fa9b06e26e9da1db0a2a8e8cc9b27cd74d
ykchat <kakutani@qj9.so-net.ne.jp>
2014-02-21 11:39:02 +0900

    for answer

---
+++ py/c549.py
@@ -0,0 +1,35 @@
+def generate_array_2d(modified, n, m):
+
+    result = []
+
+    if modified is False:
+        for i in range(n):
+            result.append([])
+            for j in range(m):
+                result[i].append(0)
+    else:
+        result = [[0] * m] * n
+
+    return result
+
+def flip_dict_items(modified, src):
+
+    dst = {}
+
+    if modified is False:
+        for key in src:
+            dst[src[key]] = key
+    else:
+        dst = {src[key]:key for key in src}
+
+    return dst
+
+if __name__ == '__main__':
+
+    print("generate a 4 * 4 array")
+    print(generate_array_2d(False, 4, 4))
+    print(generate_array_2d(True, 4, 4))
+
+    print("flip keys and values of a dictionary")
+    print(flip_dict_items(False, {1: "a", 2: "b"}))
+    print(flip_dict_items(True, {1: "a", 2: "b"}))
