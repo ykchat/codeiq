936e780c7bd59e1626dc3be3a99c2d8c2daabe96
ykchat <kakutani@qj9.so-net.ne.jp>
2014-02-21 12:09:06 +0900

    for answer

---
+++ py/c726.py
@@ -0,0 +1,29 @@
+# coding: UTF-8
+
+def search(num):
+
+    terms = [0] * num
+
+    i = 0
+    while 0 in terms:
+        i += 1
+        digits = format(i, 'b')
+        value = 0
+        for digit in digits:
+            value *= 10
+            value += int(digit) * 7
+        for j in range(num):
+            if terms[j] != 0:
+                continue
+            if value % (j+1) == 0:
+                terms[j] = value
+
+    i = 0
+    for term in terms:
+        i += 1
+        digits = str(term)
+        if digits == digits[::-1]:
+            print "n = %d のとき　%d * %d = %d" % (i, i, term/i, term)
+
+if __name__ == '__main__':
+    search(50)
